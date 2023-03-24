d5da39bfc50c17089bfcffa34ae99284f9078f9c
Ethan Wells <ethanmw2000@gmail.com>
2023-03-24 00:05:07 -0500

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ethan Wells</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Portfolio for Ethan Wells" />
+      <link rel="icon" href="/logo.svg" />
+    </>
+  )
+}
