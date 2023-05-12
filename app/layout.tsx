b7d758fca4fe8c689874592857e34fc4079b87ed
Ethan Wells <ethanmw2000@gmail.com>
2023-05-11 23:39:28 -0500

    added analytics package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { NavBar } from "@/components/NavBar";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Ethan Wells",
@@ -17,6 +18,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <Analytics />
       <head />
       <body>
         <div className="flex w-full fixed top-0 z-10">
