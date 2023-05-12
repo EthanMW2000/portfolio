1e114e31f9298824fe7beccf2a9e847304ff7a4d
Ethan Wells <ethanmw2000@gmail.com>
2023-05-11 23:07:02 -0500

    added motion to experience section

--- components/About.tsx
+++ components/About.tsx
@@ -5,114 +5,117 @@ import motion from "framer-motion";
 
 export function About() {
   return (
-    <div id="about" className="flex flex-row justify-center min-h-screen pt-24 lg:pt-0">
+    <div
+      id="about"
+      className="flex flex-row justify-center min-h-screen pt-24 lg:pt-0"
+    >
       <div className="flex flex-col justify-center content-center space-y-8">
-      <h1 className="text-center text-4xl">About Me</h1>
-      <div className="flex flex-row justify-center">
-        <Image
-          className="rounded-xl"
-          alt="my photo"
-          src="/my-photo.jpg"
-          width={275}
-          height={275}
-        />
-      </div>
-      <div className="flex flex-col lg:flex-row">
-        <div className="flex flex-col justify-center max-w-lg">
-          <div className="flex flex-col space-y-4 text-lg pb-24">
-            <p>
-              Hello! My name is Ethan Wells and I am a software engineer. For a
-              long time I have been interested in programming and over the
-              course of my college career I have found myself most interested in
-              web and app development.
-            </p>
-            <p>
-              Currently I am working as a software engineer with StaffWise. With
-              them I am developing a web application that wll give hospitals the
-              proper budgetting tools. At the same time I am completeing my
-              senior year at Wichita State University, after which I will have
-              received a B.S. in Computer Science.
-            </p>
-            <p>
-              During my spare moments I have been devoting my time to working
-              with
-              <b> Go</b> and <b>Swift</b>. Outside of programming I enjoy
-              photography, anime, and League.
-            </p>
+        <h1 className="text-center text-4xl">About Me</h1>
+        <div className="flex flex-row justify-center">
+          <Image
+            className="rounded-xl"
+            alt="my photo"
+            src="/my-photo.jpg"
+            width={275}
+            height={275}
+          />
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col justify-center max-w-lg">
+            <div className="flex flex-col space-y-4 text-lg pb-24">
+              <p>
+                Hello! My name is Ethan Wells and I am a software engineer. For
+                a long time I have been interested in programming and over the
+                course of my college career I have found myself most interested
+                in web and app development.
+              </p>
+              <p>
+                Currently I am working as a software engineer with StaffWise.
+                With them I am developing a web application that wll give
+                hospitals the proper budgetting tools. At the same time I am
+                completeing my senior year at Wichita State University, after
+                which I will have received a B.S. in Computer Science.
+              </p>
+              <p>
+                During my spare moments I have been devoting my time to working
+                with
+                <b> Go</b> and <b>Swift</b>. Outside of programming I enjoy
+                photography, anime, and League.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center max-w-lg">
+            <div className="flex flex-col space-y-4 text-lg pb-24">
+              <Marquee
+                speed={65}
+                gradient={true}
+                gradientWidth={100}
+                pauseOnHover={true}
+                autoFill={true}
+              >
+                {skills.languages.map((skill) => (
+                  <div key={skill} className=" m-2 p-2 rounded-lg">
+                    <h2 className="text-2xl">{skill}</h2>
+                  </div>
+                ))}
+              </Marquee>
+              <Marquee
+                direction="right"
+                speed={65}
+                gradient={true}
+                gradientWidth={100}
+                pauseOnHover={true}
+                autoFill={true}
+              >
+                {skills.frontend.map((skill) => (
+                  <div key={skill} className=" m-2 p-2 rounded-lg">
+                    <h2 className="text-2xl">{skill}</h2>
+                  </div>
+                ))}
+              </Marquee>
+              <Marquee
+                speed={65}
+                gradient={true}
+                gradientWidth={100}
+                pauseOnHover={true}
+                autoFill={true}
+              >
+                {skills.backend.map((skill) => (
+                  <div key={skill} className=" m-2 p-2 rounded-lg">
+                    <h2 className="text-2xl">{skill}</h2>
+                  </div>
+                ))}
+              </Marquee>
+              <Marquee
+                direction="right"
+                speed={65}
+                gradient={true}
+                gradientWidth={100}
+                pauseOnHover={true}
+                autoFill={true}
+              >
+                {skills.database.map((skill) => (
+                  <div key={skill} className=" m-2 p-2 rounded-lg">
+                    <h2 className="text-2xl">{skill}</h2>
+                  </div>
+                ))}
+              </Marquee>
+              <Marquee
+                speed={65}
+                gradient={true}
+                gradientWidth={100}
+                pauseOnHover={true}
+                autoFill={true}
+              >
+                {skills.devops.map((skill) => (
+                  <div key={skill} className=" m-2 p-2 rounded-lg">
+                    <h2 className="text-2xl">{skill}</h2>
+                  </div>
+                ))}
+              </Marquee>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col justify-center max-w-lg">
-          <div className="flex flex-col space-y-4 text-lg pb-24">
-            <Marquee
-              speed={65}
-              gradient={true}
-              gradientWidth={100}
-              pauseOnHover={true}
-              autoFill={true}
-            >
-              {skills.languages.map((skill) => (
-                <div key={skill} className=" m-2 p-2 rounded-lg">
-                  <h2 className="text-2xl">{skill}</h2>
-                </div>
-              ))}
-            </Marquee>
-            <Marquee
-              direction="right"
-              speed={65}
-              gradient={true}
-              gradientWidth={100}
-              pauseOnHover={true}
-              autoFill={true}
-            >
-              {skills.frontend.map((skill) => (
-                <div key={skill} className=" m-2 p-2 rounded-lg">
-                  <h2 className="text-2xl">{skill}</h2>
-                </div>
-              ))}
-            </Marquee>
-            <Marquee
-              speed={65}
-              gradient={true}
-              gradientWidth={100}
-              pauseOnHover={true}
-              autoFill={true}
-            >
-              {skills.backend.map((skill) => (
-                <div key={skill} className=" m-2 p-2 rounded-lg">
-                  <h2 className="text-2xl">{skill}</h2>
-                </div>
-              ))}
-            </Marquee>
-            <Marquee
-              direction="right"
-              speed={65}
-              gradient={true}
-              gradientWidth={100}
-              pauseOnHover={true}
-              autoFill={true}
-            >
-              {skills.database.map((skill) => (
-                <div key={skill} className=" m-2 p-2 rounded-lg">
-                  <h2 className="text-2xl">{skill}</h2>
-                </div>
-              ))}
-            </Marquee>
-            <Marquee
-              speed={65}
-              gradient={true}
-              gradientWidth={100}
-              pauseOnHover={true}
-              autoFill={true}
-            >
-              {skills.devops.map((skill) => (
-                <div key={skill} className=" m-2 p-2 rounded-lg">
-                  <h2 className="text-2xl">{skill}</h2>
-                </div>
-              ))}
-            </Marquee>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,24 +1,47 @@
+"use client";
 import experiences from "../assets/experience.json";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export function Experience() {
+  const [dragged, setDragged] = useState(false);
+
   return (
     <div id="experience" className="min-h-screen flex flex-row justify-center">
       <div className="flex flex-col justify-center items-center space-y-10">
         <h1 className="text-4xl">Experience</h1>
-        <div className="flex flex-row justify-center items-center space-y-4 space-x-6 text-lg">
+        <motion.button onDrag={() => setDragged(true)} drag className="text-sm font-bold">{dragged ? 'NOT ME!!' : '*try dragging*'}</motion.button>
+        <div className="flex flex-row justify-center items-center space-x-6 text-lg">
           {experiences.work.map((work) => (
-            <div key={work.title} className="rounded-xl w-[500px] h-[400px] border-2">
-              <h2 className="text-2xl text-center border-b-2 py-4">{work.title}</h2>
-              <div className="p-4">
-              <h3 className="text-xl">{work.employer}</h3>
-              <p className="text-lg">{work.descriptions.map((desc) => {
-                return desc + "\n ";
-              })}</p>
-              <p className="text-lg">{work.technologies.map((tech) => {
-                return tech + " ";
-              })}</p>
+            <motion.div
+              key={work.title}
+              whileHover={{ scale: 1.1 }}
+              drag
+              dragConstraints={{
+                top: -300,
+                left: -300,
+                right: 300,
+                bottom: 300,
+              }}
+              className="flex flex-col justify-center items-center bg-gray-500 rounded-xl w-[500px] h-[400px] "
+            >
+              <h2 className="text-3xl text-white text-center py-4">
+                {work.title}
+              </h2>
+              <div className="px-4 pt-4 h-full bg-gray-300 rounded-b-xl">
+                <h3 className="text-2xl">{work.employer}</h3>
+                <p className="text-lg">
+                  {work.descriptions.map((desc) => {
+                    return desc + "\n ";
+                  })}
+                </p>
+                <p className="text-lg">
+                  {work.technologies.map((tech) => {
+                    return tech + " ";
+                  })}
+                </p>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
