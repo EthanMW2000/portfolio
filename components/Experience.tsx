a62e4dd37b693288a12d421294a67c00831da8da
Ethan Wells <ethanmw2000@gmail.com>
2023-05-12 13:10:16 -0500

    added backdrop blur to experience section cards

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -34,50 +34,46 @@ export function Experience() {
                 top: -300,
                 bottom: 300,
               }}
-              className="flex flex-col justify-center items-center bg-gray-500 rounded-xl w-[300px] h-full md:w-[500px] xl:w-[550px] md:h-[400px]"
+              className="flex flex-col bg-neutral-50/15 rounded-xl w-[300px] h-full md:w-[500px] xl:w-[550px] md:h-[400px] px-4 backdrop-blur-sm shadow"
             >
-              <h2 className="text-xl md:text-3xl text-white text-center py-4">
+              <h2 className="text-xl md:text-3xl text-center py-4">
                 {work.title}
               </h2>
-              <div className="px-4 pt-4 w-full h-full bg-gray-300 space-y-2 rounded-b-xl pb-4">
-                <h3 className="text-lg md:text-2xl">{work.employer}</h3>
-                {work.descriptions.map((desc) => {
+
+              <h3 className="text-lg md:text-2xl">{work.employer}</h3>
+              {work.descriptions.map((desc) => {
+                return (
+                  <p key={desc} className="text-md md:text-lg">{`- ${desc}`}</p>
+                );
+              })}
+              <div className="flex flex-wrap flex-row justify-stretch">
+                {work.technologies.map((tech) => {
                   return (
-                    <p
-                      key={desc}
-                      className="text-md md:text-lg"
-                    >{`- ${desc}`}</p>
+                    <motion.p
+                      transition={{
+                        duration: 2,
+                        ease: "easeInOut",
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        repeat: Infinity,
+                        repeatDelay: 2,
+                      }}
+                      animate={{
+                        scale: [1, 1.25, 1.25, 1, 1],
+                      }}
+                      drag
+                      dragConstraints={{
+                        left: -80,
+                        right: 80,
+                        top: -80,
+                        bottom: 80,
+                      }}
+                      key={tech}
+                      className="text-md md:text-lg px-2 my-1 mx-3 bg-gray-400 rounded-lg"
+                    >
+                      {tech}
+                    </motion.p>
                   );
                 })}
-                <div className="flex flex-wrap flex-row justify-stretch">
-                  {work.technologies.map((tech) => {
-                    return (
-                      <motion.p
-                        transition={{
-                          duration: 2,
-                          ease: "easeInOut",
-                          times: [0, 0.2, 0.5, 0.8, 1],
-                          repeat: Infinity,
-                          repeatDelay: 2,
-                        }}
-                        animate={{
-                          scale: [1, 1.25, 1.25, 1, 1],
-                        }}
-                        drag
-                        dragConstraints={{
-                          left: -80,
-                          right: 80,
-                          top: -80,
-                          bottom: 80,
-                        }}
-                        key={tech}
-                        className="text-md md:text-lg px-2 my-1 mx-3 bg-gray-400 rounded-lg"
-                      >
-                        {tech}
-                      </motion.p>
-                    );
-                  })}
-                </div>
               </div>
             </motion.div>
           ))}
@@ -89,44 +85,43 @@ export function Experience() {
               initial={{ opacity: 0, y: -100 }}
               transition={{ duration: 1 }}
               whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
               key={project.name}
-              className="flex flex-col justify-center items-center bg-gray-500 rounded-xl w-[300px] h-full md:w-[500px] xl:w-[550px] md:h-[300px] m-4"
+              className="flex flex-col bg-neutral-50/15 rounded-xl w-[300px] h-full md:w-[500px] xl:w-[600px] md:h-[300px] xl:h-[275px] px-4 m-3 backdrop-blur-sm shadow"
             >
-              <h2 className="text-xl md:text-3xl text-white text-center py-4">
+              <h2 className="text-xl md:text-3xl text-center py-4">
                 {project.name}
               </h2>
-              <div className="px-4 pt-4 w-full h-full bg-gray-300 space-y-2 rounded-b-xl pb-4">
-                <h3 className="text-lg md:text-2xl">{project.type}</h3>
-                <p className="text-md md:text-lg">{project.description}</p>
-                <div className="flex flex-wrap flex-row justify-stretch">
-                  {project.technologies.map((tech) => {
-                    return (
-                      <motion.p
-                        transition={{
-                          duration: 2,
-                          ease: "easeInOut",
-                          times: [0, 0.2, 0.5, 0.8, 1],
-                          repeat: Infinity,
-                          repeatDelay: 2,
-                        }}
-                        animate={{
-                          scale: [1, 1.25, 1.25, 1, 1],
-                        }}
-                        drag
-                        dragConstraints={{
-                          left: -80,
-                          right: 80,
-                          top: -80,
-                          bottom: 80,
-                        }}
-                        key={tech}
-                        className="text-md md:text-lg px-2 my-1 mx-3 bg-gray-400 rounded-lg"
-                      >
-                        {tech}
-                      </motion.p>
-                    );
-                  })}
-                </div>
+              <h3 className="text-lg md:text-2xl">{project.type}</h3>
+              <p className="text-md md:text-lg">{project.description}</p>
+              <div className="flex flex-wrap flex-row justify-stretch">
+                {project.technologies.map((tech) => {
+                  return (
+                    <motion.p
+                      transition={{
+                        duration: 2,
+                        ease: "easeInOut",
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        repeat: Infinity,
+                        repeatDelay: 2,
+                      }}
+                      animate={{
+                        scale: [1, 1.25, 1.25, 1, 1],
+                      }}
+                      drag
+                      dragConstraints={{
+                        left: -80,
+                        right: 80,
+                        top: -80,
+                        bottom: 80,
+                      }}
+                      key={tech}
+                      className="text-md md:text-lg px-2 my-1 mx-3 bg-gray-400 rounded-lg"
+                    >
+                      {tech}
+                    </motion.p>
+                  );
+                })}
               </div>
             </motion.div>
           ))}
