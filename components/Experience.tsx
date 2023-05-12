77c0cf3eb3bfae5e55ec2c3f888575475570dd3f
Ethan Wells <ethanmw2000@gmail.com>
2023-05-11 23:35:11 -0500

    made experiences more mobile friendly

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,7 +11,7 @@ export function Experience() {
       <div className="flex flex-col justify-center items-center space-y-10">
         <h1 className="text-4xl">Experience</h1>
         <motion.button onDrag={() => setDragged(true)} drag className="text-sm font-bold">{dragged ? 'NOT ME!!' : '*try dragging*'}</motion.button>
-        <div className="flex flex-row justify-center items-center space-x-6 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-x-6 text-lg">
           {experiences.work.map((work) => (
             <motion.div
               key={work.title}
@@ -23,23 +23,21 @@ export function Experience() {
                 right: 300,
                 bottom: 300,
               }}
-              className="flex flex-col justify-center items-center bg-gray-500 rounded-xl w-[500px] h-[400px] "
+              className="flex flex-col justify-center items-center bg-gray-500 rounded-xl w-[300px] h-full md:w-[500px] md:h-[400px]"
             >
-              <h2 className="text-3xl text-white text-center py-4">
+              <h2 className="text-xl md:text-3xl text-white text-center py-4">
                 {work.title}
               </h2>
-              <div className="px-4 pt-4 h-full bg-gray-300 rounded-b-xl">
-                <h3 className="text-2xl">{work.employer}</h3>
-                <p className="text-lg">
+              <div className="px-4 pt-4 w-full h-full bg-gray-300 space-y-2 rounded-b-xl pb-4">
+                <h3 className="text-lg md:text-2xl">{work.employer}</h3>
                   {work.descriptions.map((desc) => {
-                    return desc + "\n ";
+                    return <p key={desc} className="text-md md:text-lg">{`- ${desc}`}</p>
                   })}
-                </p>
-                <p className="text-lg">
+                  <div className="flex flex-wrap flex-row justify-center space-x-2 ">
                   {work.technologies.map((tech) => {
-                    return tech + " ";
+                    return <p key={tech} className="text-md md:text-lg">{tech}</p>
                   })}
-                </p>
+                  </div>
               </div>
             </motion.div>
           ))}
