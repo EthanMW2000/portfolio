cdd867e0eb7554972dffee7388375f6d931ca4b1
Ethan Wells <ethanmw2000@gmail.com>
2023-08-03 22:11:11 -0500

    added navbar to main portfolio page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,20 @@ import { Intro } from "@/components/homepage/Intro";
 import { About } from "@/components/homepage/About";
 import { Experience } from "@/components/homepage/Experience";
 import { Contact } from "@/components/homepage/Contact";
+import { NavBar } from "@/components/homepage/NavBar";
 
 export default function Home() {
   return (
-    <main className="p-4 flex flex-col text-primary bg-secondary bg-noise font-sans">
-      <Intro />
-      <About />
-      <Experience />
-      <Contact />
-    </main>
+    <div>
+      <div className="flex w-full fixed top-0 z-10">
+        <NavBar />
+      </div>
+      <div className="p-4 flex flex-col text-primary bg-secondary bg-noise font-sans">
+        <Intro />
+        <About />
+        <Experience />
+        <Contact />
+      </div>
+    </div>
   );
 }
