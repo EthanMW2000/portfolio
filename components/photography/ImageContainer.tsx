de44760e1b03f6c8aad666352d88e66b99537d84
Ethan Wells <ethanmw2000@gmail.com>
2023-08-06 21:34:04 -0500

    added basic image container

---
+++ components/photography/ImageContainer.tsx
@@ -0,0 +1,9 @@
+export function ImageContainer() {
+  return (
+    <div className="w-[300px] h-[200px] drop-shadow-xl rounded-lg bg-gray-500">
+      <div className="w-full h-[30px] absolute bottom-0 bg-secondary/80 px-2 flex flex-row items-center">
+        <h1 className="text-lg text-primary">Date</h1>
+      </div>
+    </div>
+  );
+}
