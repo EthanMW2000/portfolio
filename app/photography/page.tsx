6b151382015c4d775b07b6923034060eb1a0ec91
Ethan Wells <ethanmw2000@gmail.com>
2023-08-24 10:21:33 -0500

    made changes to env variables and included example env

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -7,8 +7,8 @@ const s3 = new S3Client([
   {
     region: process.env.S3_BUCKET_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
     },
   },
 ]);
