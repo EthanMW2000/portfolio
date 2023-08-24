c9e95d9682df3285cc262f5e600d288942f1f9c9
Ethan Wells <ethanmw2000@gmail.com>
2023-08-23 20:40:20 -0500

    working on adding retrieval from S3 client

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -1,8 +1,45 @@
 import { ChevronRight } from "@/components/icons/ChevronRight";
 import { ImageContainer } from "@/components/photography/ImageContainer";
 import Sidebar from "@/components/photography/Sidebar";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client([
+  {
+    region: process.env.S3_BUCKET_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  },
+]);
+
+const command = new ListObjectsV2Command({
+  Bucket: process.env.S3_BUCKET_NAME,
+  Prefix: process.env.S3_BUCKET_PREFIX,
+});
+
+const returnImages = () => {
+  let images: string[] = [];
+  s3.send(command)
+      .then((data) => {
+        if (!data.Contents) return console.log("No S3 objects found");
+        data.Contents.forEach((image) => {
+          if (!image.Key) return;
+          images.push(image.Key);
+        });
+      })
+      .catch((err) => {
+        console.log("Unable to retrieve S3 objects", err);
+      });
+
+  return images;
+};
 
 export default function Photography() {
+  const images = returnImages();
+
+    
+
   return (
     <main>
       <Sidebar />
@@ -20,7 +57,14 @@ export default function Photography() {
             <ChevronRight width={24} height={24} fill="#1b2541" />
           </a>
         </div>
-        <ImageContainer />
+        {images.map((image, index) => {
+          return (
+            <ImageContainer
+              key={index}
+              url={`https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_S3_BUCKET_REGION}.amazonaws.com/${image}`}
+            />
+          );
+        })}
       </div>
     </main>
   );
