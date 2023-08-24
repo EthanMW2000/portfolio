71171732d6041b29fb4822058fda8b9470f863e2
Ethan Wells <ethanmw2000@gmail.com>
2023-08-23 20:40:45 -0500

    trying to add images to photography portfolio

--- components/photography/ImageContainer.tsx
+++ components/photography/ImageContainer.tsx
@@ -1,6 +1,9 @@
-export function ImageContainer() {
+import Image from "next/image"
+
+export function ImageContainer(props: { url: string }) {
   return (
     <div className="w-[300px] h-[200px] drop-shadow-xl rounded-lg bg-gray-500">
+      <Image src={props.url} layout="fill" alt="A photograph" />
       <div className="w-full h-[30px] absolute bottom-0 bg-secondary/80 px-2 flex flex-row items-center">
         <h1 className="text-lg text-primary">Date</h1>
       </div>
