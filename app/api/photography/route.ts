5f6f24877e1f811a0b0312ca9d5fe922366bb7bf
Ethan Wells <ethanmw2000@gmail.com>
2023-09-06 20:38:11 -0500

    generic GET response

--- app/api/photography/route.ts
+++ app/api/photography/route.ts
@@ -29,3 +29,9 @@
 //     headers: { "content-type": "application/json" },
 //   });
 // }
+
+export async function GET(request: Request) { 
+  return new Response("Hello world!", {
+    headers: { "content-type": "text/plain" },
+  });
+}
