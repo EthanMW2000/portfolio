9d71e7ecd3111d4064128eef9db8c0b34eb8cda7
Ethan Wells <ethanmw2000@gmail.com>
2023-09-15 00:07:24 -0500

    updated image element

--- components/photography/ImageContainer.tsx
+++ components/photography/ImageContainer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function ImageContainer(props: { url: string }) {
   return (
     <div className="w-[300px] h-[200px] drop-shadow-xl rounded-lg bg-gray-500">
-      <Image src={props.url} layout="fill" alt="A photograph" />
+      <img src={props.url} width={300} height={200} alt="A photograph" />
       <div className="w-full h-[30px] absolute bottom-0 bg-secondary/80 px-2 flex flex-row items-center">
         <h1 className="text-lg text-primary">Date</h1>
       </div>
