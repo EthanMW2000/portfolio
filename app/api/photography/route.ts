bdf189d3a93653f5736c43f57542eeff9edcf1a2
Ethan Wells <ethanmw2000@gmail.com>
2023-12-31 14:52:02 -0600

    using signed urls now

--- app/api/photography/route.ts
+++ app/api/photography/route.ts
@@ -1,29 +1,57 @@
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const imageClient = new S3Client({ region: process.env.S3_BUCKET_REGION });
+export const dynamic = "force-dynamic";
+
+const imageClient = new S3Client();
 const imageBucket = process.env.S3_BUCKET_NAME;
 const imagePrefix = process.env.S3_BUCKET_PREFIX;
 
-const retrieveImageCommand = new ListObjectsV2Command({
-  Bucket: imageBucket,
-  Prefix: imagePrefix,
-});
-
-async function retrieveImages() {
-  const { Contents } = await imageClient.send(retrieveImageCommand);
-  if (!Contents) {
-    throw new Error("No images found");
-  }
-  return Contents;
-}
-
 export async function GET(request: Request) {
-  const images = await retrieveImages();
-  const filteredImages = images.filter((image) => {
-    return image.Key !== imagePrefix;
-  }).map((image) => {return image.Key});
+  const allObjects = [];
+  let continuationToken;
 
-  return new Response(JSON.stringify(filteredImages), {
-    headers: { "content-type": "application/json" },
-  });
+  do {
+    const params: ListObjectsV2Command = new ListObjectsV2Command({
+      Bucket: imageBucket,
+      Prefix: imagePrefix,
+      ContinuationToken: continuationToken,
+    });
+
+    const response = await imageClient.send(params);
+    const objects = response.Contents || [];
+
+    allObjects.push(...objects);
+    continuationToken = response.NextContinuationToken;
+  } while (continuationToken);
+
+  const filteredImages = allObjects
+    .filter((image) => {
+      return image.Key !== imagePrefix;
+    })
+    .map((image) => {
+      return image.Key;
+    });
+
+  try {
+    const signedUrls = await Promise.allSettled(
+      filteredImages.map((image) => {
+        const params: GetObjectCommand = new GetObjectCommand({
+          Bucket: imageBucket,
+          Key: image,
+        });
+
+        return getSignedUrl(imageClient, params, { expiresIn: 3600 });
+      })
+    );
+
+    return new Response(JSON.stringify(signedUrls));
+  } catch (err) {
+    console.log(err);
+    return new Response("Error fetching images", { status: 500 });
+  }
 }
