afa49ed8d3b239720824306054a1617e4146bc38
Ethan Wells <ethanmw2000@gmail.com>
2024-09-09 22:17:43 -0500

    reworked imageContainer to take in image object with url and metadata details

--- components/photography/ImageContainer.tsx
+++ components/photography/ImageContainer.tsx
@@ -1,11 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 
-export function ImageContainer(props: { url: string }) {
+import { S3Object } from "@/types";
+
+export function ImageContainer({ s3Object }: { s3Object: S3Object }) {
+  const formattedDate = s3Object.metadata.DateTimeOriginal
+    ? new Date(s3Object.metadata.DateTimeOriginal).toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })
+    : "Unknown Date";
+
   return (
     <div className="w-[300px] h-[200px] drop-shadow-xl rounded-lg bg-gray-500">
-      <img src={props.url} width={300} height={200} alt="A photograph" className="rounded-lg object-none" />
-      <div className="w-full h-[30px] absolute bottom-0 bg-secondary/80 px-2 flex flex-row items-center">
-        <h1 className="text-lg text-primary">Date</h1>
+      <img
+        src={s3Object.url}
+        alt="A photograph"
+        className="rounded-lg object-contain"
+      />
+      <div className="w-full h-[30px] absolute bottom-0 bg-secondary/50 px-2 flex flex-row items-center">
+        <h1 className="text-lg text-primary-button text-shadow-outline">{formattedDate}</h1>
       </div>
     </div>
   );
